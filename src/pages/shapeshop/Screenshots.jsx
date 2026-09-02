1da27112d096429abacd5fd19216dc869b36893a
oliverwatkins <oliver.f.watkins@gmail.com>
2026-09-02 14:20:23 +0200

    Add mobile screenshots to Shape Shop screenshots page
    Captures the same 5 views (products, checkout, admin products, admin
    edit-product, admin orders) at a real mobile viewport (iPhone 12,
    390x844, via DevTools device emulation) and renders them in the
    previously-empty "Mobile" section, in their own grid variant sized for
    portrait images.

--- src/pages/shapeshop/Screenshots.jsx
+++ src/pages/shapeshop/Screenshots.jsx
@@ -1,4 +1,3 @@
-
 import "./Screenshots.scss";
 
 import img1 from "./img/shop-products.png";
@@ -7,19 +6,40 @@ import img3 from "./img/shop-admin-products.png";
 import img4 from "./img/shop-admin-edit-product.png";
 import img5 from "./img/shop-admin-orders.png";
 
+import img1Mobile from "./img/shop-products-mobile.png";
+import img2Mobile from "./img/shop-checkout-mobile.png";
+import img3Mobile from "./img/shop-admin-products-mobile.png";
+import img4Mobile from "./img/shop-admin-edit-product-mobile.png";
+import img5Mobile from "./img/shop-admin-orders-mobile.png";
+
 const screenshots = [img1, img2, img3, img4, img5];
+const mobileScreenshots = [img1Mobile, img2Mobile, img3Mobile, img4Mobile, img5Mobile];
 
 export default function Screenshots() {
   return (
-    <div className="screenshot-grid">
-      {screenshots.map((src, index) => (
-        <img
-          key={index}
-          src={src}
-          className="screenshot-image"
-          alt={`Shape Shop screenshot ${index + 1}`}
-        />
-      ))}
-    </div>
+    <>
+      <div>Desktop</div>
+      <div className="screenshot-grid">
+        {screenshots.map((src, index) => (
+          <img
+            key={index}
+            src={src}
+            className="screenshot-image"
+            alt={`Shape Shop screenshot ${index + 1}`}
+          />
+        ))}
+      </div>
+      <div>Mobile</div>
+      <div className="screenshot-grid screenshot-grid-mobile">
+        {mobileScreenshots.map((src, index) => (
+          <img
+            key={index}
+            src={src}
+            className="screenshot-image screenshot-image-mobile"
+            alt={`Shape Shop mobile screenshot ${index + 1}`}
+          />
+        ))}
+      </div>
+    </>
   );
 }
